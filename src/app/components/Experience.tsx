5b8805d6750eaa6616dbc28783d76c6dfe209292
onixChiconela <you@example.com>
2024-11-22 12:15:23 +0200

    view

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -11,6 +11,61 @@ interface ExperienceProps {
   compDesc?: string
 }
 
+// const Experience: React.FC<ExperienceProps> = ({
+//   title,
+//   companyName,
+//   duration,
+//   content,
+//   tools,
+//   toolsDesc,
+//   comp,
+//   compDesc
+// }) => {
+//   return (
+//     <div className="p-2 w-full">
+//       <div
+//         className="
+//           p-1
+//           rounded-lg
+//           border-transparent
+//           bg-clip-border
+//           bg-gradient-to-b
+//           from-white
+//           via-fuchsia-950
+//           to-black
+//           shadow-xl w-full
+
+//           mx-auto">
+//         <span className="block bg-white rounded-lg p-8">
+//           <h3 className="text-lg font-bold">{title}</h3>
+//           <p className="text-gray-700 font-semibold my-2">{companyName}</p>
+//           <p className="text-gray-500">{duration}</p>
+//           <ul className="text-gray-600 mt-2 list-disc list-inside space-y-2">
+//             {content && content.map((point, index) => (
+//               <li key={index}>{point}</li>
+//             ))}
+//           </ul>
+//           {tools ? (
+//             <li className=" mt-2 list-disc list-inside space-y-2">
+//             {tools && (<text className="font-bold ">{tools}: <text className='text-gray-600 font-normal'>{toolsDesc}</text></text>)}
+//           </li>
+//           ) : (
+//             <></>
+//           )}
+//            {comp ? (
+//             <li className="mt-2 list-disc list-inside space-y-2">
+//             {comp && (<text className="font-bold ">{comp}: <text className='text-gray-600 font-normal'>{compDesc}</text></text>)}
+//           </li>
+//           ) : (
+//             <></>
+//           )}
+
+//         </span>
+//       </div>
+//     </div>
+//   );
+// }
+
 const Experience: React.FC<ExperienceProps> = ({
   title,
   companyName,
@@ -25,18 +80,21 @@ const Experience: React.FC<ExperienceProps> = ({
     <div className="p-2 w-full">
       <div
         className="
-          p-1
-          rounded-lg
-          border-transparent
-          bg-clip-border
-          bg-gradient-to-b
-          from-white
-          via-fuchsia-950
-          to-black
-          shadow-xl w-full
-          
-          mx-auto">
-        <span className="block bg-white rounded-lg p-8">
+         p-1
+         rounded-lg
+         border-transparent
+         bg-clip-border
+         bg-gradient-to-b
+         from-white
+         via-fuchsia-950
+         to-black
+         shadow-xl
+         w-full
+         mx-auto
+         h-full
+       "
+      >
+        <span className="block bg-white rounded-lg p-8 h-full flex flex-col justify-between">
           <h3 className="text-lg font-bold">{title}</h3>
           <p className="text-gray-700 font-semibold my-2">{companyName}</p>
           <p className="text-gray-500">{duration}</p>
@@ -46,24 +104,20 @@ const Experience: React.FC<ExperienceProps> = ({
             ))}
           </ul>
           {tools ? (
-            <li className=" mt-2 list-disc list-inside space-y-2">
-            {tools && (<text className="font-bold ">{tools}: <text className='text-gray-600 font-normal'>{toolsDesc}</text></text>)}
-          </li>
-          ) : (
-            <></>
-          )}
-           {comp ? (
-            <li className="mt-2 list-disc list-inside space-y-2">
-            {comp && (<text className="font-bold ">{comp}: <text className='text-gray-600 font-normal'>{compDesc}</text></text>)}
-          </li>
-          ) : (
-            <></>
-          )}
-           
+            <div className="mt-2">
+              <strong>{tools}:</strong> <span className="text-gray-600 font-normal">{toolsDesc}</span>
+            </div>
+          ) : null}
+          {comp ? (
+            <div className="mt-2">
+              <strong>{comp}:</strong> <span className="text-gray-600 font-normal">{compDesc}</span>
+            </div>
+          ) : null}
         </span>
       </div>
     </div>
   );
-}
+};
+
 
 export default Experience;
